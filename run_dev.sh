63d3ddee7193ad01e73f488c67fc2e0b890ca124
Wicked Nick <wckdone@wckdone.com>
2019-04-24 00:54:31 +0000

    Docker

--- run_dev.sh
+++ run_dev.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-export DATABASE_URL="mysql2://wckdone:password@127.0.0.1"
+export DATABASE_URL="mysql2://root:password@mysql"
+export REDIS_URL="redis"
 export APP_ENV="development"
 
-#ruby db/create_database.rb && sequel -m db/migrations $DATABASE_URL/wckdone && ruby test/create_users.rb
+sudo bundle install
+
+sudo gem install rerun
+
+ruby db/create_database.rb && sequel -m db/migrations $DATABASE_URL/wckdone && ruby test/create_users.rb
 
 sequel -m db/migrations $DATABASE_URL/wckdone
 
